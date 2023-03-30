56a88dbfb587fc279f28722c0add937a9ef9126f
marielaure1 <edjour.marielaure@gmail.com>
2023-03-30 15:39:40 +0200

    padding a-propos

--- components/a-propos-component/index.tsx
+++ components/a-propos-component/index.tsx
@@ -8,7 +8,7 @@ const APropos: FC = () => {
             <p className="lg:text-xl sm:text-1xl text-base max-w-[700px] mb-[20px]">Lorem ipsum dolor sit amet consectetur. Lectus nulla sagittis sed vestibulum et eget suspendisse pellentesque. Vulputate quam sit habitant molestie sem tempus velit elit arcu. Velit purus est adipiscing accumsan mauris nulla sollicitudin ac nunc.</p>
             <p className="lg:text-xl sm:text-1xl text-base max-w-[700px] mb-[20px]">Felis dignissim tortor justo convallis tincidunt amet tincidunt risus non. Purus mi phasellus lobortis ornare. Odio sem fermentum vel risus nec sed. Hac massa diam tempus aenean donec nibh.</p>
 
-            <div className="italic mt-[5vw] font-times flex sm:flex-row flex-col">
+            <div className="italic mt-[5vw] mb-[5vw] font-times flex sm:flex-row flex-col">
                 <strong className="mr-[30px] lg:text-1xl text-sm">HTML - CSS - SASS</strong>
                 <strong className="mr-[30px] lg:text-1xl text-sm">Javascript - JQuery</strong>
                 <strong className="lg:text-1xl text-sm">PHP - Laravel - MySQL</strong>
